da0fa433a0f948173340e4381bcd7689a3470a15
apa <apa@mac.local>
2023-02-19 12:29:34 +0100

    pilas22.py funcionando

--- pilas/pilas22.py
+++ pilas/pilas22.py
@@ -149,32 +149,33 @@ def subirTodo_a_A():
     for _ in range(len(b)):
         pa(a,b); contador += 1
 
-def establece_lis():   # esta función solo se debería invocar una vez. Proporciona una matriz con listas de posibles lis
+def establece_lis(arr):   # esta función solo se debería invocar una vez. Proporciona una matriz con listas de posibles lis
     m2 = [[0,1],[0,-1],[0,2],[0,-2],[0,3],[0,-3],[1,-1],[1,2],[1,-2],[1,3],[1,-3],[-1,2],[-1,-2],[-1,3],[-1,-3],[2,-2],[2,3],[2,-3],[-2,3],[-2,-3],[3,-3]]
     posibles_lis = []
     for pareja in m2:   # m2 tiene 21 parejas
         x,y = pareja[0], pareja[1]
-        lis = [a[x], a[y]]
+        lis = [arr[x], arr[y]]
         posibles_lis.append(lis)
 
     m3 = [[0,1,-1],[0,1,2],[0,1,-2],[0,1,3],[0,1,-3],[0,-1,2],[0,-1,-2],[0,-1,3],[0,-1,-3],[0,2,-2],[0,2,3],[0,2,-3],[0,-2,3],[0,-2,-3],[0,3,-3],[1,-1,2],[1,-1,-2],[1,-1,3],[1,-1,-3],[1,2,-2],[1,2,3],[1,2,-3],[1,-2,3],[1,-2,-3],[1,3,-3],[-1,2,-2],[-1,2,3],[-1,2,-3],[-1,-2,3],[-1,-2,-3],[-1,3,-3],[2,-2,3],[2,-2,-3],[2,3,-3],[-2,3,-3]]
     for trio in m3:   # m3 tiene 35 trios, total len(m2)+len(m3)=21+35=56 casos posibles de lis 
         x,y,z = trio[0], trio[1], trio[2]
-        lis = [a[x], a[y], a[z]]
+        lis = [arr[x], arr[y], arr[z]]
         posibles_lis.append(lis)
+    return posibles_lis
 
 if __name__ == "__main__":    
     n = 500   # número de elementos de la pila
     limite = 5500   # para n=100 limite<700, para n=500 limite<5500
     n_intentos = [0]*56   # array que contará los intentos de tipo cero, uno, dos, tres,....
-    for i in range(100000):
+    for i in range(20):
         a = generaPilaA(n)
         #print('a:', a)
         b = []
         a_original = a[:]
         n = len(a)
+        posibles_lis = establece_lis(a)   # llama a la función que proporciona una matriz con las posibles lis de dos o tres números de la pila A
         intento = 0   # contador con los intentos probando diferentes lis
-        posibles_lis = establece_lis()   # llama a la función que proporciona una matriz con las posibles lis de dos o tres números de la pila A
         while True:
             a = a_original[:]
             b = []
@@ -197,5 +198,5 @@ if __name__ == "__main__":
             #print(f"La pila A {'SI' if sorted(a_original)==a else 'No' } está ordenada.")
             #print(f"contador: {contador}")
         if i%1000==0:
-            print(i)
+            print(i, n_intentos[:9])
     print(n_intentos)
